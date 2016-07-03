da1a1a56d65b7a57650d7ace41c3e2bc64b48180
Koen Van Geert <koen.van.geert@gmail.com>
2016-07-03 11:55:18 +0200

    added api version for summoner icons

--- client/components/common/helpers.js
+++ client/components/common/helpers.js
@@ -1,8 +1,20 @@
 import './vendor/sweetalert.min';
+import { HTTP } from 'meteor/http';
 
 export default function (Template) {
     const ddragonUrl = Meteor.settings.public.ddragon;
+    let ddragonVersion = '';
+    HTTP.get('https://ddragon.leagueoflegends.com/api/versions.json', function(error,data){
+        if(error){
+            console.log(error);
+        }
+        var versions = JSON.parse(data.content);
+        ddragonVersion = versions[0];
+    });
     Template.registerHelper( 'ddragonUrl', () => {
         return ddragonUrl;
+    });
+    Template.registerHelper( 'ddragonVersion', () => {
+        return ddragonVersion;
     });
 }
